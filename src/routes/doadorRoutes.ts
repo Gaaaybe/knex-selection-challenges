729293b1ea839c030013f474ca84048b9d58eaf2
Gaaaybe <gabenesus@gmail.com>
2024-11-14 20:45:11 -0300

    feat: adiciona rotas para o controlador Doador com métodos para criar, buscar e atualizar

---
+++ src/routes/doadorRoutes.ts
@@ -0,0 +1,8 @@
+import DoadorController from "../controllers/doadorController";
+import { Router } from "express";
+
+const doadorRoutes = Router();
+
+doadorRoutes.post("/doadores", DoadorController.createDoador);
+doadorRoutes.get("/doadores/:id", DoadorController.getDoadorById);
+doadorRoutes.put("/doadores/:id", DoadorController.updateDoador);
