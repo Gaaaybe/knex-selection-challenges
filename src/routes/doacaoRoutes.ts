a17f6c9b05c0d7b4182eff8bf2fe1fa4bc8f78ef
Gaaaybe <gabenesus@gmail.com>
2024-11-14 22:28:37 -0300

    feat: adiciona exportação de rotas de Doacao e integra com as rotas principais

--- src/routes/doacaoRoutes.ts
+++ src/routes/doacaoRoutes.ts
@@ -6,3 +6,5 @@ const doacaoRoutes = Router();
 doacaoRoutes.post("/doacoes", DoacaoController.createDoacao);
 doacaoRoutes.get("/doacoes/:id", DoacaoController.getDoacaoById);
 doacaoRoutes.put("/doacoes/:id", DoacaoController.updateDoacao);
+
+export default doacaoRoutes;
