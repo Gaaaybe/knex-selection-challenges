1f62c291db99bc5f01cb049cf2aa77c80f31765b
Gaaaybe <gabenesus@gmail.com>
2024-11-15 02:03:26 -0300

    fix: exporta as rotas doador para uso em outros módulos

--- src/routes/doadorRoutes.ts
+++ src/routes/doadorRoutes.ts
@@ -6,3 +6,5 @@ const doadorRoutes = Router();
 doadorRoutes.post("/doadores", DoadorController.createDoador);
 doadorRoutes.get("/doadores/:id", DoadorController.getDoadorById);
 doadorRoutes.put("/doadores/:id", DoadorController.updateDoador);
+
+export default doadorRoutes;
